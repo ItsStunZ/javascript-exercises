ececb67d2627c038556afba099d8d5dc15016b3e
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-03 00:41:17 +0000

    Completed 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
+    if (repeat < 0) return 'ERROR';
 
+    let newString = '';
+    for (i=0; i < repeat; i++) {
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
